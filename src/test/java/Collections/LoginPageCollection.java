cb2d7083d03920c3dfef02c8a5cadd23b5986047
emreaycicek <emreaycicek1@gmail.com>
2023-11-12 16:23:43 +0300

    Update LoginPageCollection.java

--- src/test/java/Collections/LoginPageCollection.java
+++ src/test/java/Collections/LoginPageCollection.java
@@ -2,5 +2,5 @@ package Collections;
 
 public class LoginPageCollection {
     public static String userMail = "emreaycicek10@gmail.com";
-    public static String password = "2320791Enes";
+    public static String password = "Test";
 }
